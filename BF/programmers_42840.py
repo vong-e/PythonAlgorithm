8460da59b89f86d89794bd8f298b6959b1fcc36b
vong-e <sinkim123321@gmail.com>
2020-10-11 22:46:00 +0900

    for grass

---
+++ BF/programmers_42840.py
@@ -0,0 +1,13 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42840
+
+def solution(answers):
+    math_loser_1 = [1, 2, 3, 4, 5]
+    math_loser_2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    math_loser_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
+#dlrjv이거풀어야함
+    for answer in answers:
+
+
+
+solution([1,2,3,4,5])
