1b8673f10b41d156528f35d74232c34844b67a3f
vong-e <sinkim123321@gmail.com>
2020-10-31 23:40:33 +0900

    for grass

--- BF/programmers_42840.py
+++ BF/programmers_42840.py
@@ -5,8 +5,10 @@ def solution(answers):
     math_loser_2 = [2, 1, 2, 3, 2, 4, 2, 5]
     math_loser_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
 
-#dlrjv이거풀어야함
+    answer_count = [-1]
     for answer in answers:
 
 
-solution([1,2,3,4,5])
+
+answers = [1,2,3,4,5]
+solution(answers)
