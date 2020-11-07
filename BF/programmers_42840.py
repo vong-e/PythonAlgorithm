d66fa480a66d512e38ae6873cfac08a260768718
vong-e <sinkim123321@gmail.com>
2020-11-07 23:52:19 +0900

    for grass

--- BF/programmers_42840.py
+++ BF/programmers_42840.py
@@ -6,8 +6,8 @@ def solution(answers):
     math_loser_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
 
     answer_count = [-1]
-    for answer in answers:
-
+    #for answer in answers:
+    ㅇㅕ기해ㅔ야히ㅏ
 
 
 answers = [1,2,3,4,5]
